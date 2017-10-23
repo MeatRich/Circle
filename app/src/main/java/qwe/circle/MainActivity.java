d4c09d40e33faff520d4aee55b95b5689c38e7cd
CoolEvgeny <jopoches@yandex.ru>
2017-10-24 01:10:57 +0300

    Проверка удалённого репозитория, Евгений

--- app/src/main/java/qwe/circle/MainActivity.java
+++ app/src/main/java/qwe/circle/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         statistics.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
